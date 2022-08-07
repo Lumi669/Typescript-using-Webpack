b4c572b472168bd656556009e0dbcaba78c2acec
cmjiwa <wangjinghuan@yahoo.com>
2022-08-07 19:05:45 +0300

    fix a typo, and change script file in index.html, now works well

--- dist/components/project-input.js
+++ dist/components/project-input.js
@@ -4,10 +4,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import bbb from "./base-component.js";
-import * as Validation from "../util/validation.js";
-import { Autobind as aaa } from "../decorators/autobind.js";
-import { projectState } from "../state/project-state.js";
+import bbb from "./base-component";
+import * as Validation from "../util/validation";
+import { Autobind as aaa } from "../decorators/autobind";
+import { projectState } from "../state/project-state";
 export class ProjectInput extends bbb {
     constructor() {
         super("project-input", "app", true), "user-input";
--- dist/components/project-list.js
+++ dist/components/project-list.js
@@ -4,11 +4,11 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import { Autobind } from "../decorators/autobind.js";
-import ddd from "./base-component.js";
-import { ProjectStatus } from "../models/project.js";
-import { projectState } from "../state/project-state.js";
-import { ProjectItem } from "./project-item.js";
+import { Autobind } from "../decorators/autobind";
+import ddd from "./base-component";
+import { ProjectStatus } from "../models/project";
+import { projectState } from "../state/project-state";
+import { ProjectItem } from "./project-item";
 export class ProjectList extends ddd {
     constructor(type) {
         super("project-list", "app", false, `${type}-projects`);
