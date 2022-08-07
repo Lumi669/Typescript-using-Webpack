b2a398b2596e289da31f9a6099f33e4f21aaa922
cmjiwa <wangjinghuan@yahoo.com>
2022-08-07 14:48:38 +0300

    construct entry point and output point for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,9 @@
-module.exports = {};
+const path = require("path");
+
+module.exports = {
+    entry: "./src/app.ts",
+    output: {
+        filename: "bundle.[contenthash].js",
+        path: path.resolve(__dirname, "dist")
+    }
+};
